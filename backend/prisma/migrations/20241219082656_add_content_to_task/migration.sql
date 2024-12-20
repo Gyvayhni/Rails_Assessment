7de87d9bc2fcb403e42ec5ba1db731cb7c9e5de1
Gyvayhni <ggyvayhni.14@gmail.com>
2024-12-20 15:45:19 +0800

    Create migration.sql

---
+++ backend/prisma/migrations/20241219082656_add_content_to_task/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `List` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "List_name_key" ON "List"("name");
+
+-- CreateIndex
+CREATE INDEX "Task_listId_idx" ON "Task"("listId");
