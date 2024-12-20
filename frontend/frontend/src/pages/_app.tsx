fbc4e2dcb94733cc1e42e98e1ae6189405c23834
Gyvayhni <ggyvayhni.14@gmail.com>
2024-12-20 22:38:15 +0800

    Update _app.tsx

--- frontend/frontend/src/pages/_app.tsx
+++ frontend/frontend/src/pages/_app.tsx
@@ -1,17 +1,19 @@
-// pages/_app.tsx
 import "../styles/globals.css";
-import { trpcClient } from "../utils/trpc"; // Make sure this path is correct
+import { trpcClient } from "../utils/trpc"; // Import the client from utils
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import trpc from "../utils/trpc"; // Import trpc hooks
-import { Provider } from 'jotai'; 
+import { Provider as JotaiProvider } from 'jotai'; // Rename Jotai Provider
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: any) {
   return (
     <QueryClientProvider client={queryClient}>
+      {/* TRPC Provider wraps everything */}
       <trpc.Provider client={trpcClient} queryClient={queryClient}>
-        <Component {...pageProps} />
+        <JotaiProvider>
+          <Component {...pageProps} />
+        </JotaiProvider>
       </trpc.Provider>
     </QueryClientProvider>
   );
